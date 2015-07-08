207cff6792972bb4e7d341fe3581d368e308c3f3
Romain ACCIARI <romain.acciari@gmail.com>
2015-07-08 18:24:07 +0200

    Fix ipaddress

--- manifests/account-1.pp
+++ manifests/account-1.pp
@@ -1,6 +1,8 @@
 openiosds::account {'account-1':
-  num => '1',
-  ns => 'OPENIO',
+  num        => '1',
+  ns         => 'OPENIO',
+  ipaddress  => "${ipaddress_enp0s8}",
+  port       => '6014',
   redis_host => '127.0.0.1',
   redis_port => '6379',
 }
