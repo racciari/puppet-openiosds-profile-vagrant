df16b964c161bfe84a4cd22fbb40e3120dd15575
Romain ACCIARI <romain.acciari@gmail.com>
2016-05-03 16:16:26 -0700

    vagrant-ext-swift works with de defautl vagrant box

--- vagrant-ext-swift/predeploy.sh
+++ vagrant-ext-swift/predeploy.sh
@@ -17,3 +17,19 @@ fi
 # Switching vagrant main IP into manifests
 $SED -i -e "s@VAGRANT_MAIN_VM@$VAGRANT_MAIN_VM@g" /usr/share/puppet/modules/openiosds/profiles/vagrant-ext-swift/manifests/*.pp
 
+# Install required packages
+/usr/bin/yum -y install puppet memcached python-swiftclient
+
+# Install Keystone puppet module
+/usr/bin/puppet module install stackforge-keystone
+
+# Configure memcached
+/usr/bin/systemctl enable memcached.service
+/usr/bin/systemctl start memcached.service
+
+# Default authentification variables for Keystone
+echo "export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=DEMO_PASS
+export OS_AUTH_URL=http://localhost:5000/v2.0" \
+  > ~openio/keystonerc_demo
