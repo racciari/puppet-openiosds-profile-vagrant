bbc72ba343fd195b54339e64aaa9fea0b50aa78d
Romain ACCIARI <romain.acciari@gmail.com>
2016-06-03 13:51:56 +0200

    Add predeploy

---
+++ vagrant-swift/predeploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install required packages
+/usr/bin/yum -y install puppet memcached python-swiftclient
+
+# Install Keystone puppet module
+/usr/bin/puppet module install stackforge-keystone
+
+# Configure memcached
+/usr/bin/systemctl enable memcached.service
+/usr/bin/systemctl start memcached.service
+
+# Default authentification variables for Keystone
+echo "export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=DEMO_PASS
+export OS_AUTH_URL=http://localhost:5000/v2.0" \
+  > ~openio/keystonerc_demo
